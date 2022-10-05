b255d391b853cd0685a9f8c1447f739150b298fa
Sven <mailsvb@gmail.com>
2022-10-05 23:17:00 +0200

    Add scheduler

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 $( document ).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
     action('state');
-    $('#settings-container').hide();
+    $('#heizung-container').hide();
+    $('#warmwasser-container').hide();
+    createSchedule();
 });
 
 let lastDialledNumbers = [];
@@ -25,14 +27,24 @@ $('#logout').click(function() {
 $('#exit').click(function() {
     action('exit');
 });
-$('#settings').click(function() {
+$('#heizung').click(function() {
     $('#home-container').hide();
-    $('#settings-container').show();
+    $('#warmwasser-container').hide();
+    $('#heizung-container').show();
     action('settings');
 });
-$('#back').click(function() {
-    $('#settings-container').hide();
-    $('#home-container').show();
+$('#warmwasser').click(function() {
+    $('#home-container').hide();
+    $('#heizung-container').hide();
+    $('#warmwasser-container').show();
+    action('settings');
+});
+$('#heizung-back,#warmwasser-back').each(function(){
+    $(this).click(function() {
+        $('#heizung-container').hide();
+        $('#warmwasser-container').hide();
+        $('#home-container').show();
+    });
 });
 $('#4200-set').change(() => {
     window.electronAPI.send('action', { action: 'set', id: 4200, value: parseInt($('#4200-set').val()) });
@@ -56,6 +68,127 @@ $('#settings-container input').on('change', (event) => {
     $('#preferredHotKey').prop('disabled', $('#dialFromHotKey').is(':checked') === false);
     Object.keys(obj).indexOf('value') >= 0 && window.electronAPI.send('settings', obj);
 });
+
+function createSchedule() {
+    const programs = ['ww', 'hz', 'pu'];
+    const days = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'];
+    const ids = {
+        ww: {
+            Mo: { 1: [ 3209, 3210 ], 2: [ 3211, 3212 ], 3: [ 3213, 3214 ] },
+            Di: { 1: [ 3219, 3220 ], 2: [ 3221, 3222 ], 3: [ 3223, 3224 ] },
+            Mi: { 1: [ 3229, 3230 ], 2: [ 3231, 3232 ], 3: [ 3233, 3234 ] },
+            Do: { 1: [ 3239, 3240 ], 2: [ 3241, 3242 ], 3: [ 3243, 3244 ] },
+            Fr: { 1: [ 3249, 3250 ], 2: [ 3251, 3252 ], 3: [ 3253, 3254 ] },
+            Sa: { 1: [ 3259, 3260 ], 2: [ 3261, 3262 ], 3: [ 3263, 3264 ] },
+            So: { 1: [ 3269, 3270 ], 2: [ 3271, 3272 ], 3: [ 3273, 3274 ] }
+        },
+        hz: {
+            Mo: { 1: [ 3109, 3110 ], 2: [ 3111, 3112 ], 3: [ 3113, 3114 ] },
+            Di: { 1: [ 3119, 3120 ], 2: [ 3121, 3122 ], 3: [ 3123, 3124 ] },
+            Mi: { 1: [ 3129, 3130 ], 2: [ 3131, 3132 ], 3: [ 3133, 3134 ] },
+            Do: { 1: [ 3139, 3140 ], 2: [ 3141, 3142 ], 3: [ 3143, 3144 ] },
+            Fr: { 1: [ 3149, 3150 ], 2: [ 3151, 3152 ], 3: [ 3153, 3154 ] },
+            Sa: { 1: [ 3159, 3160 ], 2: [ 3161, 3162 ], 3: [ 3163, 3164 ] },
+            So: { 1: [ 3169, 3170 ], 2: [ 3171, 3172 ], 3: [ 3173, 3174 ] }
+        },
+        pu: {
+            Mo: { 1: [ 3309, 3310 ], 2: [ 3311, 3312 ], 3: [ 3313, 3314 ] },
+            Di: { 1: [ 3319, 3320 ], 2: [ 3321, 3322 ], 3: [ 3323, 3324 ] },
+            Mi: { 1: [ 3329, 3330 ], 2: [ 3331, 3332 ], 3: [ 3333, 3334 ] },
+            Do: { 1: [ 3339, 3340 ], 2: [ 3341, 3342 ], 3: [ 3343, 3344 ] },
+            Fr: { 1: [ 3349, 3350 ], 2: [ 3351, 3352 ], 3: [ 3353, 3354 ] },
+            Sa: { 1: [ 3359, 3360 ], 2: [ 3361, 3362 ], 3: [ 3363, 3364 ] },
+            So: { 1: [ 3369, 3370 ], 2: [ 3371, 3372 ], 3: [ 3373, 3374 ] }
+        }
+    }
+    const options = {
+        0: '00:00',
+        30: '00:30',
+        100: '01:00',
+        130: '01:30',
+        200: '02:00',
+        230: '02:30',
+        300: '03:00',
+        330: '03:30',
+        400: '04:00',
+        430: '04:30',
+        500: '05:00',
+        530: '05:30',
+        600: '06:00',
+        630: '06:30',
+        700: '07:00',
+        730: '07:30',
+        800: '08:00',
+        830: '08:30',
+        900: '09:00',
+        930: '09:30',
+        1000: '10:00',
+        1030: '10:30',
+        1100: '11:00',
+        1130: '11:30',
+        1200: '12:00',
+        1230: '12:30',
+        1300: '13:00',
+        1330: '13:30',
+        1400: '14:00',
+        1430: '14:30',
+        1500: '15:00',
+        1530: '15:30',
+        1600: '16:00',
+        1630: '16:30',
+        1700: '17:00',
+        1730: '17:30',
+        1800: '18:00',
+        1830: '18:30',
+        1900: '19:00',
+        1930: '19:30',
+        2000: '20:00',
+        2030: '20:30',
+        2100: '21:00',
+        2130: '21:30',
+        2200: '22:00',
+        2230: '22:30',
+        2300: '23:00',
+        2330: '23:30',
+        2400: '24:00'
+    }
+    programs.forEach(prog => {
+        let navButton = `<nav><div class="nav nav-tabs" id="${prog}-tab" role="tablist">`
+        days.forEach((day, index) => {
+            navButton += `<button class="nav-link${index === 0 ? ' active' : ''}" id="${prog}-tab-${day.toLowerCase()}" data-bs-toggle="tab" data-bs-target="#${prog}-data-${day.toLowerCase()}" type="button" role="tab" aria-controls="${prog}-data-${day.toLowerCase()}" aria-selected="${index === 0 ? 'true' : 'false'}">${day}</button>`;
+        });
+        navButton += '</div></nav>';
+        let data = `<div class="tab-content" id="${prog}-data">`
+        Object.keys(ids[prog]).forEach((day, index) => {
+            data += `<div class="tab-pane fade${index === 0 ? ' show active' : ''}" id="${prog}-data-${day.toLowerCase()}" role="tabpanel" aria-labelledby="${prog}-tab-${day.toLowerCase()}">`;
+            data += '<table class="table table-sm"><thead><tr><th scope="col">Zeitraum</th><th scope="col">Start</th><th scope="col">Ende</th></tr></thead><tbody>';
+            Object.keys(ids[prog][day]).forEach(time => {
+                data += `<tr><th scope="row">${time}</th>`;
+                ids[prog][day][time].forEach(id => {
+                    data += `<td><select class="form-select" aria-label="${id}-set" id="${id}-set">`;
+                    Object.keys(options).forEach(option => {
+                        data += `<option value="${option}">${options[option]}</option>`;
+                    })
+                    data += '</select></td>';
+                })
+                data += '</tr>'
+            })
+            data += '</tbody></table></div>'
+        })
+        $(`#${prog}-programm`).append(`${navButton}${data}`);
+        Object.keys(ids).forEach(prog => {
+            Object.keys(ids[prog]).forEach(day => {
+                Object.keys(ids[prog][day]).forEach(time => {
+                    ids[prog][day][time].forEach(id => {
+                        $(`#${id}-set`).change(() => {
+                            window.electronAPI.send('action', { action: 'set', id, value: parseInt($(`#${id}-set`).val()) });
+                        });
+                    });
+                });
+            });
+        });
+    })
+}
 
 function action(actionType, callID) {
     var data = {
--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const net = require('net');
 const Store = require('electron-store');
 const { Helper } = require('./helper.js');
 
-app.setName('Danfoss ECL Comfort 310');
-app.setAppUserModelId('Danfoss ECL Comfort 310');
+app.setName('ECL Comfort 310 Steuerung');
+app.setAppUserModelId('ECL Comfort 310 Steuerung');
 if (process.defaultApp) {
     if (process.argv.length >= 2) {
         const res = app.setAsDefaultProtocolClient('uocti', process.execPath, [resolve(process.argv[1])]);
@@ -46,9 +46,9 @@ function initSettingsDefaults() {
 
 function createWindow () {
     globals.win = new BrowserWindow({
-        Titel: 'Danfoss ECL Comfort 310',
+        Titel: 'ECL Comfort 310 Steuerung',
         width: 510,
-        height: 550,
+        height: 700,
         icon: nativeImage.createFromPath(join(__dirname, 'images/icon.png')),
         resizable: false,
         minimizable: false,
@@ -80,7 +80,7 @@ function createWindow () {
             { label: 'Show Application', click: () => { globals.win.show(); } },
             { label: 'Quit', click: () => { app.isQuitting = true; app.quit(); } }
         ])
-        globals.tray.setToolTip('Danfoss ECL Comfort 310')
+        globals.tray.setToolTip('ECL Comfort 310 Steuerung')
         globals.tray.setContextMenu(contextMenu)
         globals.tray.on('click', () => {
             globals.win.show();
@@ -126,10 +126,142 @@ if (!gotTheLock) {
     })
 }
 
+const keepConnectionItems = Object.freeze([
+    { number: 10200, length: 0, label: "Aussentemperatur: ", method: readTemp4 },
+    { number: 10202, length: 0, label: "Vorlauftemperatur: ", method: readTemp4 },
+    { number: 10204, length: 0, label: "Rücklauftemperatur: ", method: readTemp4 },
+]);
+
 const items = Object.freeze([
     { number: 18, length: 0, label: "Typ: 087H", method: readValue },
     { number: 34, length: 1, label: "SW: ", method: readSoftware },
     { number: 35, length: 2, label: "S/N: ", method: readSerial },
+    { number: 3109, length: 0, label: "", method: readProgram },
+    { number: 3110, length: 0, label: "", method: readProgram },
+    { number: 3111, length: 0, label: "", method: readProgram },
+    { number: 3112, length: 0, label: "", method: readProgram },
+    { number: 3113, length: 0, label: "", method: readProgram },
+    { number: 3114, length: 0, label: "", method: readProgram },
+    { number: 3119, length: 0, label: "", method: readProgram },
+    { number: 3120, length: 0, label: "", method: readProgram },
+    { number: 3121, length: 0, label: "", method: readProgram },
+    { number: 3122, length: 0, label: "", method: readProgram },
+    { number: 3123, length: 0, label: "", method: readProgram },
+    { number: 3124, length: 0, label: "", method: readProgram },
+    { number: 3129, length: 0, label: "", method: readProgram },
+    { number: 3130, length: 0, label: "", method: readProgram },
+    { number: 3131, length: 0, label: "", method: readProgram },
+    { number: 3132, length: 0, label: "", method: readProgram },
+    { number: 3133, length: 0, label: "", method: readProgram },
+    { number: 3134, length: 0, label: "", method: readProgram },
+    { number: 3139, length: 0, label: "", method: readProgram },
+    { number: 3140, length: 0, label: "", method: readProgram },
+    { number: 3141, length: 0, label: "", method: readProgram },
+    { number: 3142, length: 0, label: "", method: readProgram },
+    { number: 3143, length: 0, label: "", method: readProgram },
+    { number: 3144, length: 0, label: "", method: readProgram },
+    { number: 3149, length: 0, label: "", method: readProgram },
+    { number: 3150, length: 0, label: "", method: readProgram },
+    { number: 3151, length: 0, label: "", method: readProgram },
+    { number: 3152, length: 0, label: "", method: readProgram },
+    { number: 3153, length: 0, label: "", method: readProgram },
+    { number: 3154, length: 0, label: "", method: readProgram },
+    { number: 3159, length: 0, label: "", method: readProgram },
+    { number: 3160, length: 0, label: "", method: readProgram },
+    { number: 3161, length: 0, label: "", method: readProgram },
+    { number: 3162, length: 0, label: "", method: readProgram },
+    { number: 3163, length: 0, label: "", method: readProgram },
+    { number: 3164, length: 0, label: "", method: readProgram },
+    { number: 3169, length: 0, label: "", method: readProgram },
+    { number: 3170, length: 0, label: "", method: readProgram },
+    { number: 3171, length: 0, label: "", method: readProgram },
+    { number: 3172, length: 0, label: "", method: readProgram },
+    { number: 3173, length: 0, label: "", method: readProgram },
+    { number: 3174, length: 0, label: "", method: readProgram },
+    { number: 3209, length: 0, label: "", method: readProgram },
+    { number: 3210, length: 0, label: "", method: readProgram },
+    { number: 3211, length: 0, label: "", method: readProgram },
+    { number: 3212, length: 0, label: "", method: readProgram },
+    { number: 3213, length: 0, label: "", method: readProgram },
+    { number: 3214, length: 0, label: "", method: readProgram },
+    { number: 3219, length: 0, label: "", method: readProgram },
+    { number: 3220, length: 0, label: "", method: readProgram },
+    { number: 3221, length: 0, label: "", method: readProgram },
+    { number: 3222, length: 0, label: "", method: readProgram },
+    { number: 3223, length: 0, label: "", method: readProgram },
+    { number: 3224, length: 0, label: "", method: readProgram },
+    { number: 3229, length: 0, label: "", method: readProgram },
+    { number: 3230, length: 0, label: "", method: readProgram },
+    { number: 3231, length: 0, label: "", method: readProgram },
+    { number: 3232, length: 0, label: "", method: readProgram },
+    { number: 3233, length: 0, label: "", method: readProgram },
+    { number: 3234, length: 0, label: "", method: readProgram },
+    { number: 3239, length: 0, label: "", method: readProgram },
+    { number: 3240, length: 0, label: "", method: readProgram },
+    { number: 3241, length: 0, label: "", method: readProgram },
+    { number: 3242, length: 0, label: "", method: readProgram },
+    { number: 3243, length: 0, label: "", method: readProgram },
+    { number: 3244, length: 0, label: "", method: readProgram },
+    { number: 3249, length: 0, label: "", method: readProgram },
+    { number: 3250, length: 0, label: "", method: readProgram },
+    { number: 3251, length: 0, label: "", method: readProgram },
+    { number: 3252, length: 0, label: "", method: readProgram },
+    { number: 3253, length: 0, label: "", method: readProgram },
+    { number: 3254, length: 0, label: "", method: readProgram },
+    { number: 3259, length: 0, label: "", method: readProgram },
+    { number: 3260, length: 0, label: "", method: readProgram },
+    { number: 3261, length: 0, label: "", method: readProgram },
+    { number: 3262, length: 0, label: "", method: readProgram },
+    { number: 3263, length: 0, label: "", method: readProgram },
+    { number: 3264, length: 0, label: "", method: readProgram },
+    { number: 3269, length: 0, label: "", method: readProgram },
+    { number: 3270, length: 0, label: "", method: readProgram },
+    { number: 3271, length: 0, label: "", method: readProgram },
+    { number: 3272, length: 0, label: "", method: readProgram },
+    { number: 3273, length: 0, label: "", method: readProgram },
+    { number: 3274, length: 0, label: "", method: readProgram },
+    { number: 3309, length: 0, label: "", method: readProgram },
+    { number: 3310, length: 0, label: "", method: readProgram },
+    { number: 3311, length: 0, label: "", method: readProgram },
+    { number: 3312, length: 0, label: "", method: readProgram },
+    { number: 3313, length: 0, label: "", method: readProgram },
+    { number: 3314, length: 0, label: "", method: readProgram },
+    { number: 3319, length: 0, label: "", method: readProgram },
+    { number: 3320, length: 0, label: "", method: readProgram },
+    { number: 3321, length: 0, label: "", method: readProgram },
+    { number: 3322, length: 0, label: "", method: readProgram },
+    { number: 3323, length: 0, label: "", method: readProgram },
+    { number: 3324, length: 0, label: "", method: readProgram },
+    { number: 3329, length: 0, label: "", method: readProgram },
+    { number: 3330, length: 0, label: "", method: readProgram },
+    { number: 3331, length: 0, label: "", method: readProgram },
+    { number: 3332, length: 0, label: "", method: readProgram },
+    { number: 3333, length: 0, label: "", method: readProgram },
+    { number: 3334, length: 0, label: "", method: readProgram },
+    { number: 3339, length: 0, label: "", method: readProgram },
+    { number: 3340, length: 0, label: "", method: readProgram },
+    { number: 3341, length: 0, label: "", method: readProgram },
+    { number: 3342, length: 0, label: "", method: readProgram },
+    { number: 3343, length: 0, label: "", method: readProgram },
+    { number: 3344, length: 0, label: "", method: readProgram },
+    { number: 3349, length: 0, label: "", method: readProgram },
+    { number: 3350, length: 0, label: "", method: readProgram },
+    { number: 3351, length: 0, label: "", method: readProgram },
+    { number: 3352, length: 0, label: "", method: readProgram },
+    { number: 3353, length: 0, label: "", method: readProgram },
+    { number: 3354, length: 0, label: "", method: readProgram },
+    { number: 3359, length: 0, label: "", method: readProgram },
+    { number: 3360, length: 0, label: "", method: readProgram },
+    { number: 3361, length: 0, label: "", method: readProgram },
+    { number: 3362, length: 0, label: "", method: readProgram },
+    { number: 3363, length: 0, label: "", method: readProgram },
+    { number: 3364, length: 0, label: "", method: readProgram },
+    { number: 3369, length: 0, label: "", method: readProgram },
+    { number: 3370, length: 0, label: "", method: readProgram },
+    { number: 3371, length: 0, label: "", method: readProgram },
+    { number: 3372, length: 0, label: "", method: readProgram },
+    { number: 3373, length: 0, label: "", method: readProgram },
+    { number: 3374, length: 0, label: "", method: readProgram },
     { number: 4200, length: 0, label: "Betriebsart: ", method: readBetriebsart },
     { number: 4201, length: 0, label: "Betriebsart: ", method: readBetriebsart },
     { number: 4210, length: 0, label: "Aktueller Modus: ", method: readModus },
@@ -151,12 +283,12 @@ async function handleWindowMessage(event, data) {
     Helper.logHandler(`window message [${JSON.stringify(data)}]`);
     if (data.action == 'state') {
         clearInterval(globals.interval);
-        globals.interval = setInterval(readAllValues, 10000);
+        globals.interval = setInterval(readValuesToKeepConnection, 10000);
         readAllValues();
     }
     else if (data.action == 'reload') {
         clearInterval(globals.interval);
-        globals.interval = setInterval(readAllValues, 10000);
+        globals.interval = setInterval(readValuesToKeepConnection, 10000);
         readAllValues();
     }
     else if (data.action == 'logout') {
@@ -185,7 +317,7 @@ async function handleSettingsMessage(event, data) {
 }
 
 /**
- * Connect to Danfoss ECL
+ * Connect to ECL
  */
 function connectToECL() {
     globals.win.loadFile('loading.html');
@@ -204,7 +336,18 @@ function connectToECL() {
 
 async function readAllValues() {
     for (const item of items) {
-        console.log(`reading value for id[${item.number}]`)
+        const value = await item.method(item.number, item.length)
+        const obj = {
+            action: 'state',
+            number: item.number,
+            value: `${item.label}${value}`
+        }
+        globals.win.webContents.send('data', obj);
+    }
+}
+
+async function readValuesToKeepConnection() {
+    for (const item of keepConnectionItems) {
         const value = await item.method(item.number, item.length)
         const obj = {
             action: 'state',
@@ -295,6 +438,12 @@ async function readTemp3(id) {
 async function readTemp4(id) {
     const temp = await readValue(id);
     return `${(temp / 100).toFixed(1)}°C`;
+}
+
+async function readProgram(id) {
+    const time = await readValue(id);
+    globals.win.webContents.send('settings', { key: id, value: time });
+    return time;
 }
 
 async function readBetriebsart(id) {
